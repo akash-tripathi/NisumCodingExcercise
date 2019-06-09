ba13360dd7a2ec6f78cec3087bd7abf482e32d7e
akashtrip15 <51534948+akashtrip15@users.noreply.github.com>
2019-06-09 23:02:58 +0530

    App class updated

--- src/main/java/com/nisum/codingexercise/App.java
+++ src/main/java/com/nisum/codingexercise/App.java
@@ -4,7 +4,8 @@ import com.nisum.codingexercise.model.ZipCodeRange;
 import com.nisum.codingexercise.util.ZipcodeUtility;
 
 /**
- * Hello world!
+ * This class accepts an array of 5-digit zip codes from the command line and calculate
+ * the minimun number of ranges required to represent the same restrictions as the input.
  *
  */
 public class App 
@@ -16,13 +17,13 @@ public class App
 			System.exit(1);
 		}
 	
-		//format output to flatten it as a string
+		//display input 
 		System.out.println("Input: " + ZipcodeUtility.join(args, " "));
 		
-		//run reducer which validates data and reduces the zip code array
+		//call reduceZipCodeRanges method which validates data and find the zip code array
 		ZipCodeRange[] ranges =new ZipcodeRangeCalculator().reduceZipCodeRanges(args);
 		
-		//display output as a flattened string
+		//display output 
 		System.out.println("Output: " + ZipcodeUtility.join(ranges, " "));
 	
     }
